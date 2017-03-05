8a97e80aac815be81a6105f93a21145f5a0a1ab2
Sridhar Sarnobat <ss401533@gmail.com>
2017-03-04 20:32:41 -0800

    Minor changes (untested)

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ Quick Start
 
         sh run.sh
 # websocket_file_contents
+
+	<!-- https://mvnrepository.com/artifact/org.eclipse.jetty.aggregate/jetty-all-server -->
+	<dependency>
+		<groupId>org.eclipse.jetty.aggregate</groupId>
+		<artifactId>jetty-all-server</artifactId>
+		<version>7.6.11.v20130520</version>
+	</dependency>
--- server.groovy
+++ server.groovy
@@ -1,42 +1,67 @@
 import java.io.IOException;
 
 import javax.servlet.http.HttpServletRequest;
+import org.apache.commons.io.input.ReversedLinesFileReader;
 
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.DefaultHandler;
+import org.eclipse.jetty.server.*;
+import org.eclipse.jetty.server.handler.*;
 import org.eclipse.jetty.websocket.WebSocket;
 import org.eclipse.jetty.websocket.WebSocketHandler;
 import org.eclipse.jetty.websocket.WebSocket.Connection;
 
-try {
-	Server server = new Server(8081);
-	WebSocketHandler chatWebSocketHandler = new WebSocketHandler() {
+public class WebSocketServerJetty {
+
+	private static class MyWebSocketHandler extends WebSocketHandler {
 		Connection connection;
+
+		@Override
 		public WebSocket doWebSocketConnect(HttpServletRequest request, String protocol) {
 			return new WebSocket.OnTextMessage() {
-				@Override public void onOpen(Connection conn) {
+				@Override
+				public void onOpen(Connection conn) {
 					connection = conn;
 				}
 
-				@Override public void onClose(int closeCode, String message) {
+				@Override
+				public void onClose(int closeCode, String message) {
 					connection = null;
 				}
 
-				@Override public void onMessage(String data) {
-					System.out.println("onMessage() - data: " + data);
-					try {
-						connection.sendMessage("I think I heard you say \"" + data+"\". Well, my name is ChatWebSocketServer.java");
-					} catch (IOException x) {
-						connection.close();
+				@Override
+				public void onMessage(String data) {
+				
+					File file = new File("/var/log/system.log");
+					int n_lines = 100000;
+					int counter = 0; 
+					ReversedLinesFileReader object = new ReversedLinesFileReader(file);
+					while(!object.readLine().isEmpty()  && counter < n_lines) {
+						//System.out.println(object.readLine());
+						counter++;
+						System.out.println("onMessage() - data: " + counter);
+						try {
+							connection.sendMessage(object.readLine());
+						} catch (IOException x) {
+							connection.close();
+						}
 					}
+
 				}
 			};
 		}
-	};
-	chatWebSocketHandler.setHandler(new DefaultHandler());
-	server.setHandler(chatWebSocketHandler);
-	server.start();
-	server.join();
-} catch (Throwable e) {
-	e.printStackTrace();
+
+	}
+
+	public static void main(String[] args) {
+		try {
+			Server server = new Server(8081);
+			WebSocketHandler chatWebSocketHandler = new MyWebSocketHandler();
+			chatWebSocketHandler.setHandler(new DefaultHandler());
+			server.setHandler(chatWebSocketHandler);
+			server.start();
+			server.join();
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+	}
+
 }
